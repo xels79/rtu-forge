34638e9c7d15d388b417d07ae963f85162f19775
Alexander <xel_s@mail.ru>
2026-08-27 16:20:12 +0300

    Test expanded completion

--- tests/test_completion.py
+++ tests/test_completion.py
@@ -1,6 +1,6 @@
 from rtuforge.completion import completion_candidates
 from rtuforge.config import OPTION_SPECS
-from rtuforge.helptext import COMMAND_HELP
+from rtuforge.i18n import help_topics
 
 
 def test_top_level_command_completion():
@@ -18,5 +18,20 @@ def test_option_completion_comes_from_specs():
     assert set(completion_candidates("set options ")) == {spec.name for spec in OPTION_SPECS}
 
 
+def test_option_value_completion_uses_choices():
+    assert completion_candidates("set options language ") == ["en", "ru"]
+    assert completion_candidates("set options parity e") == ["E"]
+
+
 def test_help_completion_comes_from_registry():
-    assert set(completion_candidates("help ")) == set(COMMAND_HELP)
+    assert set(completion_candidates("help ")) == set(help_topics())
+
+
+def test_history_clear_completion():
+    assert completion_candidates("history ") == ["clear"]
+    assert completion_candidates("history c") == ["clear"]
+
+
+def test_send_flag_completion():
+    assert "--decode" in completion_candidates("send ")
+    assert completion_candidates("send --d") == ["--decode"]
